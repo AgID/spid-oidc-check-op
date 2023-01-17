8e28fe2a469f412ecdd971e3773a0e0a5f45f539
damikael <michele.damico@agid.gov.it>
2023-01-17 19:09:31 +0100

    fix: conformance to Avviso SPID n.41 v.2

--- src/test/1.3.18.js
+++ src/test/1.3.18.js
@@ -5,13 +5,24 @@ class Test_1_3_18 extends TestMetadata {
     constructor(metadata) {
         super(metadata);
         this.num = "1.3.18";
-        this.description = "The metadata CAN contain the claim id_token_encryption_enc_values_supported";
-        this.validation = "self";
+        this.description = "The metadata MUST NOT contain the claim id_token_encryption_enc_values_supported  (Avviso SPID n.41 v.2)";
+        this.validation = "automatic";
     }
 
     async exec() {
         super.exec();
         
+        if(this.metadata.configuration.id_token_encryption_enc_values_supported!=null
+            && this.metadata.configuration.id_token_encryption_enc_values_supported!='') {
+
+            // if optional
+            //this.notes = "the claim id_token_encryption_enc_values_supported is not present";
+            //return true;
+
+            this.notes = this.metadata.configuration.id_token_encryption_enc_values_supported;
+            throw("the claim id_token_encryption_enc_values_supported is present");
+        } 
+
         this.notes = this.metadata.configuration.id_token_encryption_enc_values_supported;
         return true;
 
--- src/test/1.3.23.js
+++ src/test/1.3.23.js
@@ -5,22 +5,22 @@ class Test_1_3_23 extends TestMetadata {
     constructor(metadata) {
         super(metadata);
         this.num = "1.3.23";
-        this.description = "The metadata CAN contain the claim request_object_encryption_alg_values_supported";
-        this.validation = "self";
+        this.description = "The metadata MUST NOT contain the claim request_object_encryption_alg_values_supported (Avviso SPID n.41 v.2)";
+        this.validation = "automatic";
     }
 
     async exec() {
         super.exec();
         
-        if(this.metadata.configuration.request_object_encryption_alg_values_supported==null
-            || this.metadata.configuration.request_object_encryption_alg_values_supported=='') {
+        if(this.metadata.configuration.request_object_encryption_alg_values_supported!=null
+            && this.metadata.configuration.request_object_encryption_alg_values_supported!='') {
 
             // the encryption of request object is optional
-            this.notes = "the claim request_object_encryption_alg_values_supported is not present";
-            return true;
+            //this.notes = "the claim request_object_encryption_alg_values_supported is not present";
+            //return true;
 
-            //this.notes = this.metadata.configuration.request_object_encryption_alg_values_supported;
-            //throw("the claim request_object_encryption_alg_values_supported is not present");
+            this.notes = this.metadata.configuration.request_object_encryption_alg_values_supported;
+            throw("the claim request_object_encryption_alg_values_supported is present");
         } 
 
         this.notes = this.metadata.configuration.request_object_encryption_alg_values_supported;
