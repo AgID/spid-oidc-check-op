38977548119de14b6fcc423c3340a569aa23ac0b
damikael <michele.damico@agid.gov.it>
2023-01-17 18:56:35 +0100

    fix: signed_jwks_uri (Avviso SPID n.41 v.2)

--- src/test/1.3.9.js
+++ src/test/1.3.9.js
@@ -5,21 +5,39 @@ class Test_1_3_9 extends TestMetadata {
     constructor(metadata) {
         super(metadata);
         this.num = "1.3.9";
-        this.description = "The URL domain of jwks_uri value MUST match the issuer";
+        this.description = "If signed_jwks_uri is present, the URL domain of signed_jwks_uri value MUST match the issuer";
         this.validation = "automatic";
     }
 
     async exec() {
         super.exec();
         
-        if(!this.metadata.configuration.jwks_uri.toLowerCase().includes(
-            this.metadata.configuration.issuer.toLowerCase())
+        if(
+            (this.metadata.configuration.jwks==null || this.metadata.configuration.jwks=='')
+            && (this.metadata.configuration.signed_jwks_uri==null || this.metadata.configuration.signed_jwks_uri=='')
         ) {
-            this.notes = this.metadata.configuration.jwks_uri;
-            throw("the claim jwks_uri not match the issuer");
+            this.notes = this.metadata.configuration;
+            throw("both the claims jwks and signed_jwks_uri are not present");
         } 
+        
+        if(this.metadata.configuration.signed_jwks_uri!=null
+            && this.metadata.configuration.signed_jwks_uri!='') {
 
-        this.notes = this.metadata.configuration.jwks_uri;
+            if(!this.metadata.configuration.signed_jwks_uri.toLowerCase().includes(this.metadata.configuration.issuer.toLowerCase())) {
+                this.notes = this.metadata.configuration.signed_jwks_uri;
+                throw("the claim signed_jwks_uri not match the issuer");
+
+            } else {
+                this.notes = this.metadata.configuration.signed_jwks_uri;
+            }
+
+        } else {
+            this.notes = {
+                jwks: this.metadata.configuration.jwks, 
+                signed_jwks_uri: this.metadata.configuration.signed_jwks_uri
+            };
+        }
+
         return true;
 
     }
