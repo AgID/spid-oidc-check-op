d370ae22bd0b4a004d291551eb4538d75fa70fb4
damikael <michele.damico@agid.gov.it>
2023-01-17 18:55:56 +0100

    fix: homepage_uri (Avviso SPID n.41 v.2)

--- src/test/1.3.28.js
+++ src/test/1.3.28.js
@@ -5,20 +5,20 @@ class Test_1_3_28 extends TestMetadata {
     constructor(metadata) {
         super(metadata);
         this.num = "1.3.28";
-        this.description = "The metadata MUST contain the claim op_uri";
+        this.description = "The metadata MUST contain the claim homepage_uri";
         this.validation = "automatic";
     }
 
     async exec() {
         super.exec();
         
-        if(this.metadata.configuration.op_uri==null
-            || this.metadata.configuration.op_uri=='') {
-            this.notes = this.metadata.configuration.op_uri;
-            throw("the claim op_uri is not present");
+        if(this.metadata.configuration.homepage_uri==null
+            || this.metadata.configuration.homepage_uri=='') {
+            this.notes = this.metadata.configuration.homepage_uri;
+            throw("the claim homepage_uri is not present");
         } 
 
-        this.notes = this.metadata.configuration.op_uri;
+        this.notes = this.metadata.configuration.homepage_uri;
         return true;
 
     }
