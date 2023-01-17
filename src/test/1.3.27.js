2ab63db27b3f915d81f6a18e33efdd50251576c4
damikael <michele.damico@agid.gov.it>
2023-01-17 18:55:38 +0100

    fix: organization_name (Avviso SPID n.41 v.2)

--- src/test/1.3.27.js
+++ src/test/1.3.27.js
@@ -5,20 +5,20 @@ class Test_1_3_27 extends TestMetadata {
     constructor(metadata) {
         super(metadata);
         this.num = "1.3.27";
-        this.description = "The metadata MUST contain the claim op_name";
+        this.description = "The metadata MUST contain the claim organization_name";
         this.validation = "automatic";
     }
 
     async exec() {
         super.exec();
         
-        if(this.metadata.configuration.op_name==null
-            || this.metadata.configuration.op_name=='') {
-            this.notes = this.metadata.configuration.op_name;
-            throw("the claim op_name is not present");
+        if(this.metadata.configuration.organization_name==null
+            || this.metadata.configuration.organization_name=='') {
+            this.notes = this.metadata.configuration.organization_name;
+            throw("the claim organization_name is not present");
         } 
 
-        this.notes = this.metadata.configuration.op_name;
+        this.notes = this.metadata.configuration.organization_name;
         return true;
 
     }
