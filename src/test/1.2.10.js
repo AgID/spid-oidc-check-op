12d23febb5d7bbd8ef057b7232f5f9576ca6544e
lorenzo.r.sanua.external <lorenzo.r.sanua@accenture.com>
2023-02-24 15:05:13 +0100

    remove duplicated code

--- src/test/1.2.10.js
+++ src/test/1.2.10.js
@@ -1,22 +1,20 @@
-const TestMetadata = require("../server/lib/test/TestMetadata.js");
+const TestMetadata = require('../server/lib/test/TestMetadata.js');
 
 class Test_1_2_10 extends TestMetadata {
   constructor(metadata) {
     super(metadata);
-    this.num = "1.2.10";
-    this.description = "The document MUST contain the claim exp";
-    this.validation = "automatic";
+    this.num = '1.2.10';
+    this.description = 'The document MUST contain the claim exp';
+    this.validation = 'automatic';
   }
 
   async exec() {
     super.exec();
-    if (this.metadata.exp == null || this.metadata.exp == "") {
-      this.notes = this.metadata.exp;
-      throw "claim exp is not present";
-    } else {
-      this.notes = this.metadata.exp;
-      return true;
-    }
+    this.notes = this.metadata.exp;
+    if (this.notes == null || this.notes == '')
+      throw 'claim exp is not present';
+
+    return true;
   }
 }
 
