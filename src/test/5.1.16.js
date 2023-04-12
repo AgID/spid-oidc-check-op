87c93569f6725a418959e5ea1de29b597586bfa6
caterinabitonti <caterina.bitonti@accenture.com>
2023-04-12 14:59:13 +0200

    Added introspection test feat/#14

---
+++ src/test/5.1.16.js
@@ -0,0 +1,49 @@
+const TestIntrospectionRequest= require('../server/lib/test/TestIntrospectionRequest.js')
+
+
+class Test_5_1_16 extends TestIntrospectionRequest{
+    constructor(introspection){
+        super(introspection);
+        this.num="5.1.16";
+        this.description="Wrong Introspection Request:the claim jti in the client_assertion is not present";
+        this.validation="self";
+    }  
+    async exec() {
+        //this.tokenrequest.client_id = "";
+        this.introspectionrequest.code = this.authresponse.code;
+        this.introspectionrequest.code_verifier = this.authrequest.code_verifier;
+        this.introspectionrequest.grant_type = "authorization_code";
+        this.introspectionrequest.client_assertion_type = "urn:ietf:params:oauth:client-assertion-type:jwt-bearer";
+        this.introspectionrequest.redirect_uri = this.authrequest.redirect_uri;
+
+        const config_key = fs.readFileSync(path.resolve(__dirname, '../config/spid-oidc-check-op-sig.key'));
+        const keystore = jose.JWK.createKeyStore();
+
+        let key = await keystore.add(config_key, 'pem');
+
+        let header = {
+        
+        }
+
+        let iat = moment();
+        let exp = iat.clone().add(15, 'm');
+
+        let payload = JSON.stringify({ 
+            jti: Utility.getUUID(),
+            iss: this.introspectionrequest.client_id,
+            aud: this.metadata.configuration.token_endpoint,
+            iat: iat.unix(),
+            exp: exp.unix(),
+            sub: this.introspectionrequest.client_id
+        });
+
+        this.introspectionrequest.client_assertion = await jose.JWS.createSign({
+            format: 'compact',
+            alg: 'RS256',
+            fields: {...header}
+        }, key).update(payload).final();
+    }
+
+}
+
+module.exports = Test_5_1_16
---
+++ src/test/5.2.8.js
@@ -0,0 +1,49 @@
+const TestIntrospectionResponse= require('../server/lib/test/TestIntrospectionResponse.js')
+
+
+class Test_5_2_8 extends TestIntrospectionResponse{
+    constructor(introspection){
+        super(introspection);
+        this.num="5.2.8";
+        this.description="Introspection Response:the value of sub MUST be equal to the value of sub in id_token ";
+        this.validation="self";
+    }  
+    async exec() {
+        //this.tokenrequest.client_id = "";
+        this.introspectionresponse.code = this.authresponse.code;
+        this.introspectionresponse.code_verifier = this.authrequest.code_verifier;
+        this.introspectionresponse.grant_type = "authorization_code";
+        this.introspectionresponse.client_assertion_type = "urn:ietf:params:oauth:client-assertion-type:jwt-bearer";
+        this.introspectionresponse.redirect_uri = this.authrequest.redirect_uri;
+
+        const config_key = fs.readFileSync(path.resolve(__dirname, '../config/spid-oidc-check-op-sig.key'));
+        const keystore = jose.JWK.createKeyStore();
+
+        let key = await keystore.add(config_key, 'pem');
+
+        let header = {
+        
+        }
+
+        let iat = moment();
+        let exp = iat.clone().add(15, 'm');
+
+        let payload = JSON.stringify({ 
+            jti: Utility.getUUID(),
+            iss: this.introspectionrequest.client_id,
+            aud: this.metadata.configuration.token_endpoint,
+            iat: iat.unix(),
+            exp: exp.unix(),
+            sub: this.introspectionrequest.client_id
+        });
+
+        this.introspectionrequest.client_assertion = await jose.JWS.createSign({
+            format: 'compact',
+            alg: 'RS256',
+            fields: {...header}
+        }, key).update(payload).final();
+    }
+
+}
+
+module.exports = Test_5_2_8
