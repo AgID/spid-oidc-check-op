3028a8e0219e2d49f28fccb3d3be0f18f1ddaf89
MalatiniLuca <luca.malatini@accenture.com>
2023-04-17 13:07:55 +0200

    fixxed test 2.5.1

--- src/test/2.5.1.js
+++ src/test/2.5.1.js
@@ -1,30 +1,31 @@
-const TestAuthResponse = require('../server/lib/test/TestAuthResponse.js');
-const Utility = require('../server/lib/utils');
+const { access } = require("fs");
+const TestAuthResponse = require("../server/lib/test/TestAuthResponse.js");
+const Utility = require("../server/lib/utils");
 
 class Test_2_5_1 extends TestAuthResponse {
+  errorValuePermissed = [
+    "access_denied",
+    "invalid_client",
+    "invalid_request",
+    "invalid_scope",
+    "server_error",
+    "temporary_unavailable",
+  ];
   constructor(metadata, authrequest, authresponse) {
     super(metadata, authrequest, authresponse);
-    this.num = '2.5.1';
+    this.num = "2.5.1";
     this.description =
       'the value of error MUST be one of ["access_denied", "invalid_client", "invalid_request", "invalid_scope", "server_error", "temporary_unavailable"]';
-    this.validation = 'automatic';
+    this.validation = "automatic";
   }
 
   exec() {
     super.exec();
-    if (
-      this.authresponse.error.toString() != 'access_denied' ||
-      this.authresponse.error.toString() != 'invalid_client' ||
-      this.authresponse.error.toString() != 'invalid_request' ||
-      this.authresponse.error.toString() != 'invalid_scope' ||
-      this.authresponse.error.toString() != 'server_error' ||
-      this.authresponse.error.toString() != 'temporary_unavailable'
-    ) {
-      this.notes = this.authresponse.error;
-      return true;
+    if (!this.errorValuePermissed.includes(this.authresponse.error)) {
+      throw 'The value of error is not one of "access_denied", "invalid_client", "invalid_request", "invalid_scope", "server_error", "temporary_unavailable"';
     } else {
       this.notes = this.authresponse.error;
-      throw 'The value of error is not one of "access_denied", "invalid_client", "invalid_request", "invalid_scope", "server_error", "temporary_unavailable"';
+      return true;
     }
   }
 }
