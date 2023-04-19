2687ca0fb7513593e185e21a898ffe2b0395821d
MalatiniLuca <luca.malatini@accenture.com>
2023-04-19 10:02:45 +0200

    added missing test for userinfo

---
+++ src/test/4.1.0.js
@@ -0,0 +1,20 @@
+const TestUserinfoRequest = require("../server/lib/test/TestUserinfoRequest.js");
+
+class Test_4_1_0 extends TestUserinfoRequest {
+  constructor(metadata, authrequest = {}, authresponse = {}, tokenrequest = {}, tokenresponse = {}) {
+    super(metadata, authrequest, authresponse, tokenrequest, tokenresponse);
+    this.num = "4.1.0";
+    this.description = "the request is sent using HTTP method different from GET";
+    this.validation = "self";
+  }
+
+  async exec() {
+    super.exec();
+    this.notes = this.authrequest.method;
+    if (this.notes != "GET") throw "request is not using HTTP GET method";
+
+    return true;
+  }
+}
+
+module.exports = Test_4_1_0;
