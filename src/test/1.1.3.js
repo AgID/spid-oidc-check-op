1ff19b5fb76b6db6586131cbb110c2a78e01c380
damikael <michele.damico@agid.gov.it>
2023-10-19 18:12:25 +0200

    fix: 1.1.* if openid-federation

--- src/test/1.1.3.js
+++ src/test/1.1.3.js
@@ -12,13 +12,22 @@ class Test_1_1_3 extends TestMetadata {
 
     async exec() {
         super.exec();
-        let response = await axios.get(this.metadata.url);
-        if(!response.headers['content-type'].includes('application/json')) {
-            this.notes = response.headers['content-type'];
-            throw("Content-Type is not 'application/json'");
-        } else {
-            this.notes = response.headers['content-type'];
+
+        if(this.metadata.type=='federation') {
+
+            this.notes = "N/A - metadata is provided as openid-federation";
             return true;
+
+        } else {
+
+            let response = await axios.get(this.metadata.url);
+            if(!response.headers['content-type'].includes('application/json')) {
+                this.notes = response.headers['content-type'];
+                throw("Content-Type is not 'application/json'");
+            } else {
+                this.notes = response.headers['content-type'];
+                return true;
+            }
         }
     }
 
