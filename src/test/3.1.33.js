3e746bf26355fb6f0f4b8c39f3ec55dd4cd6cc9e
Michele D'Amico <michele.damico@linfaservice.it>
2024-03-25 11:39:38 +0100

    Update 3.1.33.js

--- src/test/3.1.33.js
+++ src/test/3.1.33.js
@@ -1,57 +1,55 @@
 const fs = require("fs");
 const path = require("path");
-const moment = require("../server/node_modules/moment");
-const jose = require("../server/node_modules/node-jose");
-const TestTokenRequest = require("../server/lib/test/TestTokenRequest.js");
-const Utility = require("../server/lib/utils.js");
-const config_rp = require("../config/rp.json");
-class Test_3_1_33 extends TestTokenRequest {
-  constructor(metadata, authrequest = {}, authresponse = {}, tokenrequest) {
-    super(metadata, authrequest, authresponse, tokenrequest);
-    this.num = "3.1.33";
-    this.description = "Wrong Token Request:if grant_type is 'refresh_token', the value of refresh_token is not valid";
-    this.validation = "self";
-  }
+const moment = require('../server/node_modules/moment');
+const jose = require('../server/node_modules/node-jose');
+const TestRefreshTokenRequest = require('../server/lib/test/TestRefreshTokenRequest.js');
+const Utility = require('../server/lib/utils.js');
+const config_rp = require('../config/rp.json');
 
-  async exec() {
-    this.tokenrequest.client_id = config_rp.client_id;
-    this.tokenrequest.code = this.authresponse.code;
-    this.tokenrequest.code_verifier = this.authrequest.code_verifier;
-    this.tokenrequest.grant_type = "refresh_token";
-    this.tokenrequest.refresh_token = "NOT_VALID";
-    this.tokenrequest.client_assertion_type = "urn:ietf:params:oauth:client-assertion-type:jwt-bearer";
-    this.tokenrequest.redirect_uri = this.authrequest.redirect_uri;
+class Test_3_1_33 extends TestRefreshTokenRequest {
 
-    const config_key = fs.readFileSync(path.resolve(__dirname, "../config/spid-oidc-check-op-sig.key"));
-    const keystore = jose.JWK.createKeyStore();
+    constructor(metadata, authrequest={}, authresponse={}, tokenrequest={}, tokenresponse={}, refreshtokenrequest={}) {
+        super(metadata, authrequest, authresponse, tokenrequest, tokenresponse, refreshtokenrequest);
+        this.num = "3.1.33";
+        this.description = "Wrong Token Request: if grant_type is 'refresh_token', the value of refresh_token is not valid";
+        this.validation = "self";
+    }
 
-    let key = await keystore.add(config_key, "pem");
+    async exec() {
+        this.refreshtokenrequest.client_id = config_rp.client_id;
+        this.refreshtokenrequest.refresh_token = 'NOT_VALID';
+        this.refreshtokenrequest.grant_type = "refresh_token";
+        this.refreshtokenrequest.client_assertion_type = "urn:ietf:params:oauth:client-assertion-type:jwt-bearer";
+        this.refreshtokenrequest.redirect_uri = this.authrequest.redirect_uri;
 
-    let header = {};
+        const config_key = fs.readFileSync(path.resolve(__dirname, '../config/spid-oidc-check-op-sig.key'));
+        const keystore = jose.JWK.createKeyStore();
 
-    let iat = moment();
-    let exp = iat.clone().add(15, "m");
+        let key = await keystore.add(config_key, 'pem');
 
-    let payload = JSON.stringify({
-      jti: Utility.getUUID(),
-      iss: this.tokenrequest.client_id,
-      aud: this.metadata.configuration.token_endpoint,
-      iat: iat.unix(),
-      exp: exp.unix(),
-      sub: this.tokenrequest.client_id,
-    });
+        let header = {
+        
+        }
 
-    this.tokenrequest.client_assertion = await jose.JWS.createSign(
-      {
-        format: "compact",
-        alg: "RS256",
-        fields: { ...header },
-      },
-      key
-    )
-      .update(payload)
-      .final();
-  }
+        let iat = moment();
+        let exp = iat.clone().add(15, 'm');
+
+        let payload = JSON.stringify({ 
+            jti: Utility.getUUID(),
+            iss: this.tokenrequest.client_id,
+            aud: this.metadata.configuration.token_endpoint,
+            iat: iat.unix(),
+            exp: exp.unix(),
+            sub: this.tokenrequest.client_id
+        });
+
+        this.refreshtokenrequest.client_assertion = await jose.JWS.createSign({
+            format: 'compact',
+            alg: 'RS256',
+            fields: {...header}
+        }, key).update(payload).final();
+    }
+
 }
 
-module.exports = Test_3_1_33;
+module.exports = Test_3_1_33 
