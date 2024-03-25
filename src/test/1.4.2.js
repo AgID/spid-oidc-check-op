90572ccfb3916b35d36f4f65e51d2e3c6eba1dcc
damikael (Michele D'Amico) <michele.damico@linfaservice.it>
2024-03-25 16:06:25 +0000

    fix merge

---
+++ src/test/1.4.2.js
@@ -0,0 +1,26 @@
+const TestMetadata = require('../server/lib/test/TestMetadata.js');
+
+class Test_1_4_2 extends TestMetadata {
+  constructor(metadata) {
+    super(metadata);
+    this.num = '1.4.2';
+    this.description =
+      'The metadata SHOULD contain the claim grant_types_supported';
+    this.validation = 'self';
+  }
+
+  async exec() {
+    super.exec();
+
+    if (this.metadata.configuration.grant_types_supported == null) {
+      this.notes =
+        "the claim grant_types_supported is not present, it's recommended but not mandatory";
+      return true;
+    }
+
+    this.notes = this.metadata.configuration.grant_types_supported;
+    return true;
+  }
+}
+
+module.exports = Test_1_4_2;
