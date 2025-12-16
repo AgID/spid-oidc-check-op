aed15d96589a04cdaa4b6ee9d89bed303ebe71b9
damikael <michele.damico@linfaservice.it>
2025-12-16 16:26:07 +0100

    fix: 2.5.1

--- src/test/2.5.1.js
+++ src/test/2.5.1.js
@@ -6,23 +6,31 @@ class Test_2_5_1 extends TestAuthResponse {
   errorValuePermissed = [
     "access_denied",
     "invalid_client",
+    "unauthorized_client",
     "invalid_request",
     "invalid_scope",
     "server_error",
     "temporary_unavailable",
+    "unsupported_response_type",
+    "login_required",
+    "consent_required",
+    "request_uri_not_supported",
+    "registration_not_supported",
+    "invalid_request_object"
   ];
+
   constructor(metadata, authrequest, authresponse) {
     super(metadata, authrequest, authresponse);
     this.num = "2.5.1";
     this.description =
-      'the value of error MUST be one of ["access_denied", "invalid_client", "invalid_request", "invalid_scope", "server_error", "temporary_unavailable"]';
+      'the value of error MUST be one of ' + JSON.stringify(this.errorValuePermissed, null, 3);
     this.validation = "automatic";
   }
 
   exec() {
     super.exec();
     if (!this.errorValuePermissed.includes(this.authresponse.error)) {
-      throw 'The value of error is not one of "access_denied", "invalid_client", "invalid_request", "invalid_scope", "server_error", "temporary_unavailable"';
+      throw 'The value of error is not one of ' + JSON.stringify(this.errorValuePermissed, null, 3);
     } else {
       this.notes = this.authresponse.error;
       return true;
